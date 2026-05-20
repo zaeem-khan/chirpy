00e794da3e55d68431eba790f3f57b9df631c5bc
zaeem-khan <hafizzaeemkhan@gmail.com>
2026-05-20 11:27:51 -0500

    Implement middleware for file server metrics and add reset/metrics handlers

--- main.go
+++ main.go
@@ -1,16 +1,33 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
 
 func main() {
 	const filepathRoot = "."
 	const port = "8080"
+	apiCfg := &apiConfig{}
+
 	mux := http.NewServeMux()
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))))
 	mux.HandleFunc("/healthz", handlerReadiness)
+	mux.HandleFunc("/metrics", apiCfg.getHitsHandler)
+	mux.HandleFunc("/reset", apiCfg.resetHitsHandler)
 
 	srv := &http.Server{
 		Addr:    ":" + port,
@@ -19,6 +36,16 @@ func main() {
 
 	log.Printf("Serving files from %s on port %s\n", filepathRoot, port)
 	log.Fatal(srv.ListenAndServe())
+}
+
+func (cfg *apiConfig) resetHitsHandler(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.Store(0)
+	fmt.Fprintf(w, "Hits reset to 0")
+}
+
+func (cfg *apiConfig) getHitsHandler(w http.ResponseWriter, r *http.Request) {
+	hits := cfg.fileserverHits.Load()
+	fmt.Fprintf(w, "Hits: %d", hits)
 }
 
 func handlerReadiness(w http.ResponseWriter, r *http.Request) {
